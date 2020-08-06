e760d463a342ad5ff47497639eb018006d5a81ac
“Eric <enguyen2@yahoo.com>
2020-08-05 21:52:28 -0700

    add product info screen

---
+++ app/src/main/java/com/example/grocerydelivery/ProductInfo.kt
@@ -0,0 +1,23 @@
+package com.example.grocerydelivery
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.grocerydelivery.data.FoodItem
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class ProductInfo : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.product_info, container, false)
+    }
+}
--- app/src/main/java/com/example/grocerydelivery/RestaurantsAdapter.kt
+++ app/src/main/java/com/example/grocerydelivery/RestaurantsAdapter.kt
@@ -3,6 +3,7 @@ package com.example.grocerydelivery
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.grocerydelivery.data.FoodItem
 import kotlinx.android.synthetic.main.a_single_restaurant_row.view.*
@@ -23,12 +24,16 @@ class RestaurantsAdapter(private val foodItems: List<FoodItem>) :
         holder.bind(foodItem)
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         fun bind(foodItem: FoodItem) {
             itemView.foodItemTitle.text = foodItem.name
             itemView.foodPrice.text = "Price: $${foodItem.price}"
             if (foodItem.isHighlyRated) {
                 itemView.highlyRatedIcon.visibility = View.VISIBLE
+            }
+
+            view.mainPhoto.setOnClickListener {
+                view.findNavController().navigate(R.id.action_FirstFragment_to_productInfo)
             }
         }
     }
